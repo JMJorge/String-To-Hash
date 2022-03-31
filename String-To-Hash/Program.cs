754d8c42841ef76fdc1da9d4e6ae3f7bc38725b3
JJ <36696963+JMJorge@users.noreply.github.com>
2022-03-31 20:48:23 +0100

    Update Program.cs

--- String-To-Hash/Program.cs
+++ String-To-Hash/Program.cs
@@ -66,7 +66,3 @@ namespace String_To_Hash
         }
     }
 }
-// The example displays the following output:
-//    The SHA256 hash of Hello World! is: 7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069.
-//    Verifying the hash...
-//    The hashes are the same.
